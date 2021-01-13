0057852fbda4ed7d8d62bcf9f1ff3f9427680d5e
prezz <>
2021-01-13 17:54:36 +0100

    Removed spaces

--- app/src/main/java/net/prezz/mpr/service/PlaybackService.java
+++ app/src/main/java/net/prezz/mpr/service/PlaybackService.java
@@ -344,11 +344,11 @@ public class PlaybackService extends Service {
             } else if (CMD_VOL_UP.equals(action)) {
                 sendControlCommand(new VolumeUpCommand(VolumeButtonsHelper.getVolumeAmount(context)));
             } else if (CMD_PREV.equals(action)) {
-                sendControlCommand( new PreviousCommand());
+                sendControlCommand(new PreviousCommand());
             } else if (CMD_PLAY_PAUSE.equals(action)) {
-                sendControlCommand( new PlayPauseCommand());
+                sendControlCommand(new PlayPauseCommand());
             } else if (CMD_NEXT.equals(action)) {
-                sendControlCommand( new NextCommand());
+                sendControlCommand(new NextCommand());
             } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 player.setStatusListener(playerInfoRefreshListener);
             } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
